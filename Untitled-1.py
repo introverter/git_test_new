d373808140720d1c40478a659072e030107b45a9
mmnt <ldvperm@gmail.com>
2023-01-31 18:54:56 +0500

    start a new project, main logic

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,23 @@
-print('hello, git!')
-print('wow!')
-print('this is amazing!')
+def payment_check():
+    pass
+
+all_sets_prices = []
+
+
+amount_of_sets = int(input())
+
+for set in range(amount_of_sets):
+
+    price_and_amount = {}
+    amount_of_items = int(input())
+    prices = [int(i) for i in input().split(' ')]
+
+    for item in prices:
+        if item not in price_and_amount:
+            all_sets_prices.append(item*(prices.count(item)//3*2 + prices.count(item)%3))
+            price_and_amount[item] = prices.count(item)
+
+    #all_sets_prices.append(price_and_amount)
+
+for set in all_sets_prices:
+    print(set)
